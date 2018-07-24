cb71dc857909bc84fd54cf5c4fbfd467ca1e7232
happyo <happyojones@gmail.com>
2018-07-24 22:12:01 +0800

    add 2-1

---
+++ ch02/example/2-1-1.scm
@@ -0,0 +1,58 @@
+(load "../../ch01/example/1-2-5-GCD.scm")
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define x (cons 1 2))
+
+(car x)
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+;; (define make-rat cons)
+
+(define numer car)
+
+(define denom cdr)
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+
+(print-rat one-half)
+
+
+(make-rat 1 2)
+
+(define one-third (make-rat 1 3))
+
+(print-rat (add-rat one-half one-third))
+
+(print-rat (mul-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
---
+++ ch02/exercise/2-1.scm
@@ -0,0 +1,4 @@
+(define (make-rat n d)
+  (if (< 0 d)
+      (cons (- n) (- d))
+      (cons n d)))
