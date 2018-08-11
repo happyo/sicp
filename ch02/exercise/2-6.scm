2aa49a81714ca4b254c37128f8ed266010c423cd
happyo <happyojones@gmail.com>
2018-08-11 21:14:08 +0800

    add: 2-6

--- ch02/exercise/2-6.scm
+++ ch02/exercise/2-6.scm
@@ -1,1 +1,18 @@
-()
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;(define one (lambda (f) (lambda (x) (f ((zero f) x)))))
+;(define one (lambda (f) (lambda (x) (f ((lambda (x) x) x)))))
+(define one (lambda (f) (lambda (x) (f x))))
+
+;; (define two (add-1 one))
+;; (define two (lambda (f) (lambda (x) (f ((one f) x)))))
+;; (define two (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x)))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+
+
