79e3ec95ccb97f714291a2aeaf9d43fd1de6f2fa
happyo <happyojones@gmail.com>
2019-06-20 10:34:52 +0800

    add 2-77

---
+++ ch02/exercise/2-77.scm
@@ -0,0 +1,3 @@
+;; 因为 之前complex的package里面没有对应的方法
+
+;; 两次，第一次去掉complex 的tag，第二次去掉rectangular的tag
---
+++ ch02/exercise/2-78.scm
@@ -0,0 +1,8 @@
+(define (type-tag x)
+  'scheme-number)
+
+(define (contents x)
+  x)
+
+(define (attach-tag x)
+  x)
